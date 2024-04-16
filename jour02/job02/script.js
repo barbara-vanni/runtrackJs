41e6808140fe5c04d2a73bcec9e809cd884fe234
Dhjali <barbara.vanni@laplateforme.io>
2024-04-16 11:45:35 +0200

    [ADD] add and remove citation when button clicked

---
+++ jour02/job02/script.js
@@ -0,0 +1,12 @@
+function showhide() {
+    let article = document.getElementById('article');
+
+    if (article){
+        article.remove();
+    } else {
+        article = document.createElement('article');
+        article.id = 'article';
+        article.textContent = "L'important n'est pas la chute, mais l'atterrissage.";
+        document.body.appendChild(article);
+    }
+}
