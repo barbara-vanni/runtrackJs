177c0ca128ea42489ecc40b67995e6309e54237f
Dhjali <barbara.vanni@laplateforme.io>
2024-04-22 11:26:47 +0200

    [ADD] shuffle arc en ciel

--- jour03/job02/script.js
+++ jour03/job02/script.js
@@ -1,23 +1,62 @@
+$(document).ready(function() {
+    const $arcEnCiel = $('#arc-en-ciel');
+    const $images = $arcEnCiel.children('.image');
+    const $melangerButton = $('#melanger');
+    const $message = $('#message');
+    const $dropzone = $('#dropzone');
 
-
-$(document).ready(function(){
-    $('#shuffleButton').click(function(){
-        let images = $('#images img');
-        images.sort(function() { return 0.5 - Math.random();
+    function verifierOrdre() {
+        const imagesDansZone = $dropzone.find('.image').length;
+        if (imagesDansZone !== 6) {
+            $message.text('Vous avez perdu');
+            $message.css('color', 'red');
+            return;
+        }
+        
+        let ordreCorrect = true;
+        $dropzone.find('.image').each(function(index) {
+            const indexAttendu = index + 1;
+            const indexImage = parseInt($(this).attr('data-index'));
+            if (indexImage !== indexAttendu) {
+                ordreCorrect = false;
+                return false;
+            }
         });
-        $('#images').html(images);
-        draggableImg();
-    });
 
-    function draggableImg(){
-        $('.draggable').draggable({revert: 'invalid'});
+        if (ordreCorrect) {
+            $message.text('Vous avez gagné');
+            $message.css('color', 'green');
+        } else {
+            $message.text('Vous avez perdu');
+            $message.css('color', 'red');
+        }
     }
 
+    $melangerButton.on('click', function() {
+        const $shuffledImages = $images.toArray().sort(() => Math.random() - 0.5);
+        $arcEnCiel.empty().append($shuffledImages);
+        $message.text('');
 
-    draggableImg();
+        $images.draggable({
+            axis: 'y, x',
+            revert: true,
+            revertDuration: 0,
+            start: function() {
+                $(this).addClass('dragging');
+            },
+            stop: function() {
+                $(this).removeClass('dragging');
+            }
+        });
 
+        $dropzone.droppable({
+            drop: function(event, ui) {
+                const $droppedImage = ui.draggable;
+                $droppedImage.appendTo($(this));
+                verifierOrdre(); 
+            }
+        });
 
-    // $('#images').sortable();
-
-}); 
-
+        $dropzone.find('.image').removeClass('dragging').draggable('destroy');
+    });
+});
